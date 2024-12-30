dc1e89a15f6c510a5e91de582eca8ff650dc03f3
MartinDelhaye <delhayemar1@gmail.com>
2024-12-30 10:26:40 +0100

    Update mentions-legales.php
    ajout hébergeur et créateur du site

--- mentions-legales.php
+++ mentions-legales.php
@@ -21,10 +21,15 @@ include ("config/config.php");
             <h2>1. Informations légales</h2>
             <p>Le site <strong>AD PERSONAL TRAINING</strong> est édité par <strong>Deschamp Arnaud</strong>, président de la société AD PERSONAL TRAINING, immatriculée sous le numéro SIREN <strong>949 102 503</strong> et domiciliée à l'adresse suivante :</p>
             <p><strong>72 Grande Rue de Saint Clair, 69300 Caluire-et-Cuire</strong></p>
-            <p>Numéro de téléphone : <strong>[Numéro de téléphone]</strong></p>
-            <p>Adresse email : <strong>[Adresse email]</strong></p>
+            <p>Numéro de téléphone : <strong>06 64 70 26 46</strong></p> 
+            <p>Adresse email : <strong>Arnaud.deschamp@outlook.fr</strong></p>
             <p>Le directeur de la publication est : <strong>Deschamp Arnaud</strong>.</p>
-            <p>Le site est hébergé par : <strong>[Nom de l’hébergeur]</strong>, situé à <strong>[Adresse de l’hébergeur]</strong>, avec le numéro de téléphone <strong>[Numéro de téléphone de l’hébergeur]</strong>.</p>
+            <p>Le site a été créé par : <strong>Delhaye Martin</strong>, agissant en tant que particulier.</p>
+            <p>Pour toute question ou demande relative au site, vous pouvez contacter le créateur à l'adresse suivante : <strong>delhayemar1@gmail.com</strong> ou par téléphone au <strong>06 24 52 81 84</strong>.</p>
+            <p>Pour plus d'informations sur le créateur, visitez : <a href="https://martin-delhaye.fr" target="_blank">martin-delhaye.fr</a>.</p>
+            <p>Le code et le design du site ont été réalisés par <strong>Martin Delhaye</strong> et sont protégés par le droit de la propriété intellectuelle. Toute reproduction ou utilisation sans autorisation préalable est interdite.</p>
+            <p>Le site est hébergé par : <strong>InfinityFree</strong>, situé à <strong>Kwikstaartlaan 42, Unit G1517, 3704 GS Zeist, The Netherlands</strong>. InfinityFree est une initiative indépendante dédiée à l’hébergement gratuit. Ils ne proposent pas de support direct par téléphone ou chat pour les services gratuits. Pour plus d’informations, veuillez visiter leur site officiel : <a href="https://www.infinityfree.net" target="_blank">infinityfree.net</a>.</p>
+            <p>Pour signaler un abus, vous pouvez les contacter par e-mail à <a href="mailto:report-abuse@infinityfree.com">report-abuse@infinityfree.com</a>.</p>
         </section>
 
         <section>
